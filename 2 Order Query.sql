5883b3a8231d9b23aa6cfcc2d62476c19b7e2e00
harshag3104 <148677355+harshag3104@users.noreply.github.com>
2023-12-24 10:53:58 +0530

    Update and rename 2 Order Query to 2 Order Query.sql

--- 2 Order Query.sql
+++ 2 Order Query.sql
@@ -1,5 +1,5 @@
 1)
-SELECT COUNT(*) 
+SELECT COUNT(*) as Above_Average_Grades
 FROM Customer 
 WHERE Grade > (
 SELECT AVG(Grade) 
@@ -7,38 +7,30 @@ FROM Customer
 WHERE City = 'Banglore');
 
 2)
-SELECT S.Name, S.Salesman_id, COUNT(C.Customer_id) AS CustomerCount 
+SELECT S.Name,COUNT(C.Customer_id) AS CustomerCount 
 FROM Salesman S 
 JOIN Customer C ON S.Salesman_id = C.Salesman_id 
-GROUP BY S.Name, S.Salesman_id 
+GROUP BY S.Name
 HAVING COUNT(C.Customer_id) > 1;
 
 3)
-SELECT S.Name, S.City 
+SELECT S.Name, S.City, 'Customer Exists' as Customer_Status
 FROM Salesman S 
 WHERE S.Salesman_id IN (
 SELECT DISTINCT Salesman_id FROM Customer) 
 UNION 
-SELECT S.Name, S.City 
+SELECT S.Name, S.City, 'No Customer Exists' as Customer_Status
 FROM Salesman S 
 WHERE S.Salesman_id NOT IN (
 SELECT DISTINCT Salesman_id 
 FROM Customer);
-
-//**Without union**//
-
-SELECT S.Salesman_id, S.City,
-CASE WHEN C.Salesman_id IS NOT NULL THEN 'Exists' ELSE 'Not Exists' END AS Customer_Status
-FROM Salesman S
-LEFT JOIN Customer C ON S.Salesman_id = C.Salesman_id;
-
 
 4)
 CREATE VIEW MaxOrdersSalesman AS 
 SELECT Salesman_id, Purchase_Amt 
 FROM Product    
 WHERE Purchase_Amt= (
-SELECT MAX(Purchase_Amt) 
+SELECT MAX(O.Purchase_Amt) 
 FROM Product O   
 WHERE O.Ord_Date='2022-12-03');
 
@@ -52,7 +44,6 @@ DELETE FROM Salesman WHERE Salesman_id = 1000;
 6)
 CREATE INDEX idx_customer_id ON Customer (Customer_id);
 
+show indexes from Customer;
 
-
-
-
+drop index idx_customer_id on Customer;
