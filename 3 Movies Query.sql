2574b3b9d893e01df96fe77527f9626a6b649a17
harshag3104 <148677355+harshag3104@users.noreply.github.com>
2023-12-24 14:27:25 +0530

    Update 3 Movies Query.sql

--- 3 Movies Query.sql
+++ 3 Movies Query.sql
@@ -1,17 +1,14 @@
 1)
-select Mov_title
-from Movies 
+select Mov_title from Movies 
 where Dir_id=(
-select Dir_id 
-from Director 
+select Dir_id from Director 
 where Dir_Name="Hitchcock");
 
 2)
-select Mov_title from 
-Movies where Mov_id in(
-select Act_id 
-from Movies_cast 
-group by Act_id 
+select Mov_title from Movies 
+where Mov_id in( 
+select Mov_id from Movies_cast  
+group by Mov_id  
 having count(*)>=2);
 
 3)
@@ -23,12 +20,11 @@ GROUP BY A.Act_name
 HAVING COUNT(DISTINCT CASE WHEN M.Mov_year > '2020-01-01' THEN M.Mov_id END) > 0 AND COUNT(DISTINCT CASE WHEN M.Mov_year < '2002-10-01' THEN M.Mov_id END) > 0;
 
 4)
-select M.Mov_Title,R.Rev_stars 
-from Rating R 
-join Movies M on R.Mov_id=M.Mov_id 
-where M.Mov_id in (
-select Mov_id from Rating)
-order by M.Mov_title;
+SELECT M.Mov_title, R.Rev_stars 
+FROM Movies M 
+JOIN Rating R ON M.Mov_id = R.Mov_id 
+WHERE R.Rev_stars IS NOT NULL                               ( else use >=1 )  
+ORDER BY M.Mov_title;
 
 5)
 update Rating
