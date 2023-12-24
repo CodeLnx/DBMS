87b28c8c784b230bfac53f5f305a9fe4a069fbcd
IRONMAN <148677355+ironman3103@users.noreply.github.com>
2023-12-24 23:43:34 +0530

    Update 5 Company Query.sql

--- 5 Company Query.sql
+++ 5 Company Query.sql
@@ -1,1 +1,9 @@
 1)
+
+2)
+SELECT E.Name, P.Pname, E.Salary, E.Salary * 1.1 AS NewSalary
+FROM Employee E 
+JOIN Works_on W ON E.SSN = W.SSN 
+JOIN Project P ON W.Pno = P.Pno 
+WHERE P.Pname = 'IOT';
+
