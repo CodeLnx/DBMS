0747c59bbb56aed08e42b88f22c4a435c638e287
IRONMAN <148677355+ironman3103@users.noreply.github.com>
2024-01-08 08:06:20 +0530

    Update 6 Call Detail.sql

--- 6 Call Detail.sql
+++ 6 Call Detail.sql
@@ -1,4 +1,5 @@
 --create statement
+
 create table Call_detail(
 Phone_number int(10), 
 Call_start timestamp,
@@ -10,14 +11,17 @@ partition p1 values less than(unix_timestamp('2022-01-01 00.00.00')) ,
 partition p2 values less than(unix_timestamp('2023-01-01 00.00.00')) );
 
 --to create indexes 
+
 create index call_idx on Call_detail(Phone_number,Call_start);
 
 --insert statement
+
 insert into Call_detail values(123,'2022-12-01 01.02.00',02,'OFFICE');
 insert into Call_detail values(123,'2022-6-01 03.02.00',02,'OFFICE');
 insert into Call_detail values(123,'2020-6-01 03.02.00',02,'OFFICE');
 
 --show the table and partitions
+
 select * from Call_detail;
 
 select * from Call_detail partition(p1);
@@ -25,7 +29,18 @@ select * from Call_detail partition(p1);
 select * from Call_detail partition(p2);
 
 --to show the Description of the Table
+
 desc Call_detail;
 
 --to show the Indexes of the Table
+
 show indexes from Call_detail;
+
+--to add another partitions
+
+alter table Call_detail add partition (partition p3 values less than (unix_timestamp('2023-02-01 00.00.00')));
+
+--to remove partitions
+
+ alter table Call_detail drop  partition p3;
+
